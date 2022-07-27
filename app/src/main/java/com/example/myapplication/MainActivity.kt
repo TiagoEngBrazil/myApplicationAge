30acba7406d8255edd18bdbff4297b186f40a058
Tiago Lima Pimenta <102493965+TiagoEngBrazil@users.noreply.github.com>
2022-07-26 21:51:02 -0300

    evolução!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        supportActionBar?.hide()
+    }
+
+    fun calculate() {
+    }
+
+    override fun onClick(v: View) {
+        TODO("Not yet implemented")
     }
 }
